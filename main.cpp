7c818c7501be88e5c944aebfb246ecd8bc029c78
mattiyee <mattiyee610@gmail.com>
2024-12-04 02:40:33 -0500

    Issues with previous merge, fixed GH-3 GH-10 GH-15

--- main.cpp
+++ main.cpp
@@ -208,31 +208,23 @@ map<string, Student*> parseStudentData(const string& file_name) {
 }
 
 int main() {
-    cout << "Starting." << endl;
     // generateStudents(100000, "/Users/matti/CLionProjects/careerplus/generate_students.txt");
-
-    /*map<string, Course*> course_catalog = parseCourseData("/Users/catherinewu/Downloads/careerplus-main/course_data.txt");
-    map<string, Course*> course_catalog = parseCourseData("/Users/catherinewu/Downloads/careerplus-main/course_data.txt");
-    map<string, Student*> student_database = parseStudentData("/Users/catherinewu/Downloads/careerplus-main/generate_students.txt");
-    map<string, Student*> student_database = parseStudentData("/Users/catherinewu/Downloads/careerplus-main/generate_students_temp.txt");
     // cout << "complete" << endl;
     // student_database["00008015"]->Register("MAC2311", course_catalog, 1);
-    User new_user;
-    new_user.initializeData();
-    cout << student_database["00008015"]->Register("MAC2311", course_catalog, 1) << endl;  // should fail due to missing prereq
-    new_user.mainMenu();*/
-
-
     // map<string, Course*> course_catalog = parseCourseData("/Users/catherinewu/Downloads/careerplus-main/course_data.txt");
     // map<string, Student*> student_database = parseStudentData("/Users/catherinewu/Downloads/careerplus-main/generate_students.txt");
+
     map<string, Course*> course_catalog = parseCourseData("/Users/matti/CLionProjects/careerplus/course_data.txt");
     map<string, Student*> student_database = parseStudentData("/Users/matti/CLionProjects/careerplus/generate_students.txt");
     cout << "Parsing successfully complete." << endl << endl;
+    User new_user;
+    new_user.initializeData();
+    new_user.mainMenu();
 
-    cout << student_database["00008015"]->Register("MAC2311", course_catalog, 1) << endl;  // should fail due to missing prereq
-    cout << student_database["00008015"]->Register("MAC2312", course_catalog, 1) << endl;  // should fail due to missing prereq
-    cout << student_database["00008015"]->Register("PHY2048", course_catalog, 3) << endl;  // should fail due to overlap in times
-    cout << student_database["00008015"]->Unenroll("MAC2311", course_catalog) << endl;
+    // cout << student_database["00008015"]->Register("MAC2311", course_catalog, 1) << endl;  // should fail due to missing prereq
+    // cout << student_database["00008015"]->Register("MAC2312", course_catalog, 1) << endl;  // should fail due to missing prereq
+    // cout << student_database["00008015"]->Register("PHY2048", course_catalog, 3) << endl;  // should fail due to overlap in times
+    // cout << student_database["00008015"]->Unenroll("MAC2311", course_catalog) << endl;
 
     return 0;
 }
